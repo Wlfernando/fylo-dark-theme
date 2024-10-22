0f5db4c2701466ed5d61198c2d02300a886ac5bd
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-10-22 15:08:46 -0600

    Feat: insert Service into the DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css'
 import Header from './components/header/Header'
 import Hero from './components/hero/Hero'
+import Service from './components/service/Service'
 
 function App() {
   return (
     <>
       <Header />
-      <main>
+      <main className='landing-page'>
         <Hero />
+        <Service />
       </main>
     </>
   )
