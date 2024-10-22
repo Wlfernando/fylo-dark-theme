50fe8b8c5a47517ea10eba7833ffb870ef3bbe36
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-10-22 14:48:32 -0600

    Feat: add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+
+const resolve = (route: string) => path.resolve(__dirname, route)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@Assets': resolve('./src/assets'),
+    }
+  }
 })
