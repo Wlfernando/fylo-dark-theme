2dfe46c7a0e319d59bae808dcb2e915b913e8f93
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-10-26 19:13:50 -0600

    Feat: insert the toaster and the logic for accessibility-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import AccessibilityForm from './components/accessibility-form/AccessibilityForm'
 import Footer from './components/footer/Footer'
@@ -7,10 +7,12 @@ import Hero from './components/hero/Hero'
 import Service from './components/service/Service'
 import SuccessCases from './components/success-cases/SuccessCases'
 import Tour from './components/tour/Tour'
+import Toaster from './components/toaster/Toaster'
 
 type DivSelector = HTMLElement | null
 
 function App() {
+  const [toaster, setToaster] = useState<boolean>(false);
   const heroRef = useRef<DivSelector>(null);
   const landingPageRef = useRef<DivSelector>(null);
   
@@ -31,7 +33,8 @@ function App() {
         <Service />
         <Tour />
         <SuccessCases />
-        <AccessibilityForm />
+        <AccessibilityForm send={() => setToaster(true)} />
+        {toaster && <Toaster remove={() => setToaster(false) }/>}
       </main>
       <Footer />
     </>
